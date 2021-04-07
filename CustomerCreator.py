db048831b1296d388341fe6686f058d6dd9da62c
gabrieleaquaro <gabriele.aquaro@mail.polimi.it>
2021-04-07 17:46:51 +0200

    CustomerType implementation and configuration file added

---
+++ CustomerCreator.py
@@ -0,0 +1,8 @@
+class CustomerCreator() :
+
+    def __init__(self, file_path) :
+        self.file_path = file_path
+    
+    def generate_types(): 
+        #Returns a vector with the CustomerTypes element
+        
--- CustomerType.py
+++ CustomerType.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-class CustomerType():
+class Customer():
 
-    def __init__(self, clicks_coefficient, cost_coefficient, conversion_rate_matrix, comeback_vector):
+    def __init__(self,id, clicks_coefficient, cost_coefficient, conversion_rate_matrix, comeback_vector):
+        self.class_id = id
         self.clicks_coefficient = clicks_coefficient
         self.cost_coefficient = cost_coefficient   
         self.conversion_rate_matrix = conversion_rate_matrix
@@ -17,13 +18,26 @@ class CustomerType():
         return bid / (self.cost_coefficient * (1 - np.exp(-bid)))
 
     def comeback_probability(self, times):
-        # Return a probability of coming back for the user based on the class, extreme points are default.
+        #Return a probability of coming back for the user based on the class, extreme points are default.
         if times <= 1: return 1.0
         if times > 14 and < 25 return 0.05
         if times >= 25 return 0.00
         return self.comeback_vector[times - 2]
 
     def conversion_rate(self, price):
-        #TODO : similar thing of above, take the two value of the price above and below the given one, and iterpolate.
-        return 0.5
+        #Calculate the conversion rate given the conversion rate matrix linearly interpolating the points.
+        #First element of conversion matrix should be [0, 1.0]
+        if price <= 0 :
+            raise Exception("Value Error: Price can't be zero or negative.")
+        
+        i = 0
+        while self.conversion_rate_matrix[i][0] < price :
+            i+=1
+        
+        xa = self.conversion_rate_matrix[i-1][0]
+        xb = self.conversion_rate_matrix[i][0]
+        ya = self.conversion_rate_matrix[i-1][1]
+        yb = self.conversion_rate_matrix[i][1]
+
+        return (((price - xb)/(xa -xb)) * ya) - (((price - xa)/(xa -xb)) * yb)
 
