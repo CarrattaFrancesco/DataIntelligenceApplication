42a7323988988fd836b7fe4842537bdbaa8d339b
gabrieleaquaro <gabriele.aquaro@mail.polimi.it>
2021-04-08 18:20:09 +0200

    work in progress

--- customer.py
+++ customer.py
@@ -2,25 +2,25 @@ import numpy as np
 
 class Customer():
 
-    def __init__(self,id, clicks_coefficient, cost_coefficient, conversion_rate_matrix, comeback_vector):
+    def __init__(self,id, clicks_coefficient, slope_coefficient, conversion_rate_matrix, comeback_vector):
         self.class_id = id
         self.clicks_coefficient = clicks_coefficient
-        self.cost_coefficient = cost_coefficient   
+        self.slope_coefficient = slope_coefficient
         self.conversion_rate_matrix = conversion_rate_matrix
         self.comeback_vector = comeback_vector
     
     def clicks(self, bid):
         #Calculates the clicks as function of the bid for this customer type
-        return self.clicks_coefficient * (1 - np.exp(-bid)) 
+        return self.clicks_coefficient * (1 - np.exp(-self.slope_coefficient*bid)) 
 
     def cost_per_click(self, bid): 
-        #Computes the cost per click
-        return bid / (self.cost_coefficient * (1 - np.exp(-bid)))
+        #Computes the cost per click as 
+        return bid / self.clicks(bid)
 
     def comeback_probability(self, times):
         #Return a probability of coming back for the user based on the class, extreme points are default.
         if times < 1:
-            raise Exception("Value Error: Price can't be zero or negative.")
+            raise Exception("Value Error: Times can't be zero or negative.")
         if times >= 20: return 0.00
         return self.comeback_vector[times - 1]
 
--- customerManager.py
+++ customerManager.py
@@ -4,18 +4,18 @@ import numpy as np
 
 class CustomerManager():
 
-    def __init__(self, file_path, noise_variances = np.ones(shape = (4))) :
+    def __init__(self, file_path, noise_variance = 0.05) :
         self.customers = [] 
-        self.noise_variances = noise_variances
+        self.noise_variance = noise_variance
         json_file = open(file_path)
-        classes = json.load(json_file)["classes"]
-        for c in classes: 
-            self.customers.append(Customer(id = c["class-id"],
-                                          clicks_coefficient = c["properties"]["clicks_coefficient"] , 
-                                          cost_coefficient = c["properties"]["cost_coefficient"],
-                                          conversion_rate_matrix = c["properties"]["conversion_rate_matrix"], 
-                                          comeback_vector = c["properties"]["comeback_vector"]))
-    
+        self.classes = json.load(json_file)["classes"]
+        for c in self.classes: 
+            self.customers.append(Customer(id = c["class_id"],
+                                          clicks_coefficient = c["clicks_coefficient"] ,
+                                          slope_coefficient = c["slope_coefficient"],
+                                          conversion_rate_matrix = c["conversion_rate_matrix"], 
+                                          comeback_vector = c["comeback_vector"]))
+        
     def __get_by_id(self, id):
         for c in self.customers:
             if c.class_id == id:
@@ -23,14 +23,28 @@ class CustomerManager():
         raise Exception("Invalid ID")
     
 
-    def clicks(self,class_id, bid):
-        return np.clip(self.__get_by_id(class_id).clicks(bid) + np.random.normal(0, self.noise_variances[0]), a_min = 0, a_max = None)
+    def clicks(self,class_id, bid, noise = True):
+        res = self.__get_by_id(class_id).clicks(bid)
+        if not noise : 
+            return res 
+        return  np.clip(res + np.random.normal(0, res * self.noise_variance ), a_min = 0, a_max = None)
 
-    def cost_per_click(self, class_id, bid): 
-        return np.clip(self.__get_by_id(class_id).cost_per_click(bid) + np.random.normal(0, self.noise_variances[1]), a_min = 0, a_max = None)
+    def cost_per_click(self, class_id, bid, noise = True): 
+        res = self.__get_by_id(class_id).cost_per_click(bid) 
+        if not noise : 
+            return res 
+        return np.clip(res + np.random.normal(0, res * self.noise_variance ), a_min = 0, a_max = None)
 
-    def comeback_probability(self, class_id, times):
-        return np.clip(self.__get_by_id(class_id).comeback_probability(times) + np.random.normal(0, self.noise_variances[2]), a_min = 0, a_max = 1)
+    def comeback_probability(self, class_id, times, noise = True):
+        res = self.__get_by_id(class_id).comeback_probability(times)
+        if not noise : 
+            return res 
+        return np.clip( res + np.random.normal(0, res * self.noise_variance ), a_min = 0, a_max = 1)
 
-    def conversion_rate(self, class_id, price):
-        return np.clip(self.__get_by_id(class_id).conversion_rate(price) + np.random.normal(0, self.noise_variances[3]), a_min = 0, a_max = 1)
+    def conversion_rate(self, class_id, price, noise = True):
+        res = self.__get_by_id(class_id).conversion_rate(price)
+        if not noise : 
+            return res 
+        return np.clip( res + np.random.normal(0, sres * self.noise_variance ), a_min = 0, a_max = 1)
+
+
