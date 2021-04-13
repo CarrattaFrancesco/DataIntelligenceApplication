939855624524c147346eb7d8420f7fb3e0631252
MoroniFabio <fabio.moroni01@universitadipavia.it>
2021-04-13 21:06:15 +0200

    Update and Fix for Task 2

--- learners.py
+++ learners.py
@@ -9,7 +9,7 @@ class Learner_1():
         self.noise_std = noise
         self.theta = theta
         self.l = l
-
+        
 
     def fit(self,X,Y):
         kernel = C(self.theta, (1e-03, 1e3)) * RBF(1, (1e-3, 1e3)) 
@@ -19,3 +19,4 @@ class Learner_1():
 
     def predict(self,X):
         return self.model.predict(X)
+
