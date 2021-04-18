2d4e7370dc8c6c509706e5fc9880b63867fc9f61
gabrieleaquaro <gabriele.aquaro@mail.polimi.it>
2021-04-18 20:16:06 +0200

    exp5 : fixing

--- environment.py
+++ environment.py
@@ -3,8 +3,8 @@ from customerManager import CustomerManager
 
 class Environment():
 
-    def __init__(self,file_path="./config/customer_classes.json"):
-        self.cManager = CustomerManager(file_path = file_path, noise_variance= 0.01)
+    def __init__(self,file_path="./config/customer_classes.json", noise_variance = 0.05):
+        self.cManager = CustomerManager(file_path = file_path, noise_variance= noise_variance  )
 
     def round(self,bids,price, noise = True):
         #return an array (Uc1, Uc2, Uc3)
