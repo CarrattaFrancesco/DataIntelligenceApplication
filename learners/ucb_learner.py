4d9429599578e94b6155a0716f4a1c7a0382d06b
gabrieleaquaro <gabriele.aquaro@mail.polimi.it>
2021-06-24 11:10:57 +0200

    UCB fixing

--- learners/ucb_learner.py
+++ learners/ucb_learner.py
@@ -10,15 +10,17 @@ class UCB1(Learner):
 
     def pull_arm(self):
         if self.t < self.n_arms:
-            return self.t 
+            return  self.t 
         upper_bound = self.empirical_means + self.confidence
-        return np.random.choice(np.where(upper_bound == upper_bound.max())[0])
+        return np.argmax(upper_bound)
     
     def update(self, pulled_arm, reward):
         self.t += 1
         self.rewards_per_arm[pulled_arm].append(reward)
         self.collected_rewards.append(reward)
-        self.empirical_means[pulled_arm] = (self.empirical_means[pulled_arm] * (self.t-1) + reward)/self.t
+        times_pulled = len(self.rewards_per_arm[pulled_arm])
+        self.empirical_means[pulled_arm] = (self.empirical_means[pulled_arm] * (times_pulled-1) + reward)/times_pulled
         for a in range(self.n_arms):
             number_pulled = max(1, len(self.rewards_per_arm[a]) ) 
             self.confidence[a] = (2*np.log(self.t) /number_pulled)**0.5
+       
